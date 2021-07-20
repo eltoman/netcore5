ed0fcb3f18d8cb58c41da739249a42ca16ffa5e8
Elton Manoel <eltonmds@usp.br>
2021-07-20 20:18:51 -0300

    Add simple project init

---
+++ simple-api-net-core-5/FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> Filmes = new();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            Filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public List<Filme> BuscaTodos()
+        {
+            return Filmes;
+        }
+    }
+}
---
+++ simple-api-net-core-5/FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage ="O campo Titulo é Obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é Obrigatório")]
+        public string Diretor { get; set; }
+        [Required(ErrorMessage = "O campo Genero é Obrigatório")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "Min 1 max 600")]
+        public int Duracao { get; set; }
+    }
+}
